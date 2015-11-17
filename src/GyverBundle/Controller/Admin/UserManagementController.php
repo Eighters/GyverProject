acb3f3f465dc5eaecb47cfb559faac2078a5a013
Thibaut Gauvin <gauvin.thibaut83@gmail.com>
2015-11-17 20:57:21 +0100

    Add List user form Admin

---
+++ src/GyverBundle/Controller/Admin/UserManagementController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace GyverBundle\Controller\Admin;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserManagementController extends Controller
+{
+    /**
+     * @route
+     */
+
+    /**
+     * Return the view to manage user
+     *
+     * @Route("/admin/overview", name="admin_overview")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userRepository = $em->getRepository('GyverBundle:User');
+
+        $users = $userRepository->findAll();
+
+        return $this->render('Admin/overview.html.twig', array('users' => $users));
+    }
+}
