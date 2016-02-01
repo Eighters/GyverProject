a080c1dc40f500c95d1c37499ecc4065924e8d96
Thibaut-gauvin <gauvin.thibaut83@gmail.com>
2016-02-01 18:52:44 +0100

    Update sidebar & Add logo img

--- app/Resources/js/gyver_main.js
+++ app/Resources/js/gyver_main.js
@@ -94,6 +94,15 @@ $(document).ready(function(){
     /**
      * Initialize user dropdown in sidebar nav
      */
-    $('.user-infos').dropdown();
+    $('.user-infos').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrain_width: false,
+            hover: false,
+            gutter: 0,
+            belowOrigin: true,
+            alignment: 'left'
+        }
+    );
 
 });
