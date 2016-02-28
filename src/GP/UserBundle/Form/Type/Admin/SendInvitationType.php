46f08ef23347b71194d5f2a3b0103509ce511ea1
Thibaut-gauvin <gauvin.thibaut83@gmail.com>
2016-02-28 20:51:11 +0100

    Fix Insight Minor Issue

--- src/GP/UserBundle/Form/Type/Admin/SendInvitationType.php
+++ src/GP/UserBundle/Form/Type/Admin/SendInvitationType.php
@@ -4,7 +4,6 @@ namespace GP\UserBundle\Form\Type\Admin;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 /**
